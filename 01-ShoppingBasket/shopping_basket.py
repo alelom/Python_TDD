856ffb785ae559d0888fbcb4c494f33b902357c4
Alessio Lombardi <alomba02@mail.bbk.ac.uk>
2021-06-12 11:35:31 +0100

    Functional implementation of Basket's total

--- 01-ShoppingBasket/shopping_basket.py
+++ 01-ShoppingBasket/shopping_basket.py
@@ -1,10 +1,11 @@
 import dataclasses
 from typing import List
 from item import Item
+from functools import reduce
 
 @dataclasses.dataclass 
 class Basket: #dataclass is used for classes used mostly for holding data.
     items: List[Item] # This syntax uses dataclass to add an `items` property w/ getters and constructor injection. `item` is of the generic python `list` type. 
     
     def total(self):
-        return 0
+        return reduce(lambda subTotal, item: subTotal + item.unit_price * item.quantity, self.items, 0)
