3e1a55343a9967326e2e8053c9add924dd6346d1
Alessio Lombardi <alomba02@mail.bbk.ac.uk>
2021-06-12 10:07:46 +0100

    Adding the test function in.

--- shopping_basket_test.py
+++ shopping_basket_test.py
@@ -1,4 +1,12 @@
 import unittest
+import dataclasses
+
+@dataclasses.dataclass 
+class Basket(object): #dataclass is used for classes used mostly for holding data.
+    items: list # This syntax uses dataclass to add an `items` property w/ getters and constructor injection. `item` is of the generic python `list` type. 
+    
+    def total(self):
+        return 0
 
 class ShoppingBasketTest(unittest.TestCase):
     def test_empty_basket(self):
