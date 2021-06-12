55c42a273f9b1b8440a29f0934929467fbc161dd
Alessio Lombardi <alomba02@mail.bbk.ac.uk>
2021-06-12 11:35:47 +0100

    Final test for 01-basket example

--- 01-ShoppingBasket/shopping_basket_test.py
+++ 01-ShoppingBasket/shopping_basket_test.py
@@ -20,6 +20,9 @@ class Test_ShoppingBasket(unittest.TestCase):
         basket = Basket([Item(100.0, 1), Item(100.0, 1)])
         self.assertEqual(basket.total(), 200.0)
         
+    def test_two_items_quantity_two(self):
+        basket = Basket([Item(100.0, 2), Item(100.0, 2)])
+        self.assertEqual(basket.total(), 400.0)
 
 if __name__ == '__main__':
     unittest.main()
