e971031a71ee6712d61d85520ba91209910f3d51
Alessio Lombardi <alomba02@mail.bbk.ac.uk>
2021-06-12 10:07:35 +0100

    minor correction

--- shopping_basket_test.py
+++ shopping_basket_test.py
@@ -1,5 +1,6 @@
 import unittest
 
-class ShoppingBasketTest(unittest):
+class ShoppingBasketTest(unittest.TestCase):
     def test_empty_basket(self):
         basket = Basket([]) # passing an empty list "of items" as argument.
+        self.assertEqual(basket.total(), 0)
